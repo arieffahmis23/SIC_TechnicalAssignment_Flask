b5cb0c8a1ccb73f80c78a499b9a1356db5428c1b
arieffahmis23 <arieffahmi23@gmail.com>
2024-06-15 07:43:27 +0700

    Add files via upload
    Diperlukan perbaikan pada Flask

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Define the base variable globally
+base = []
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    global base
+    data = request.get_json()
+    temp_data = data['temp_data']
+    hum_data = data['temp_data']
+    intensitas = data['intensitas']
+    pengunjung = data['pengunjung']
+    SubData = [temp_data, hum_data, intensitas, pengunjung]
+
+    # Update the global base variable with received data
+    base = SubData
+
+    # Log the received data
+    print("Data diterima")
+    print("- Suhu:", temp_data, "°C, Kelembaban:", hum_data, "%")
+    print("- Intensitas Cahaya:", intensitas,", lux :", intensitas, ", Pengunjung:", pengunjung)
+    
+    # Send response
+    response = {'message': 'Data diterima'}
+    return jsonify(response), 200
+
+@app.route('/data', methods=['GET'])
+def dapat_data():
+    global base
+    # Return the base data as a JSON response
+    return jsonify({'Temperature': base[0], 'Humidity': base[1], 'Intensitas Cahaya': base[2], 'Pengunjung' : base [3]}) if base else jsonify({'message': 'No data available'})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ sketch.ino
@@ -0,0 +1,139 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <DHT.h>
+#include <LiquidCrystal_I2C.h>
+
+// Replace with your network credentials
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+// Your server URL
+const char* serverUrl = "http://localhost:5000/data"; // Change to your server's local IP address
+
+#define DHTPIN 25  // GPIO pin where the DHT22 is connected
+#define ldr 36
+#define pir 16
+#define Relay_Lamp 2
+#define Relay_AC 17
+
+#define DHTTYPE DHT22
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+DHT dht(DHTPIN, DHTTYPE);
+
+const float GAMMA = 0.7;
+const float RL10 = 50;
+int pengunjung = 0;
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  lcd.init();
+  lcd.backlight();
+  pinMode(pir, INPUT);
+  pinMode(ldr, INPUT);
+  pinMode(Relay_Lamp,OUTPUT);
+  pinMode(Relay_AC, OUTPUT);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to Wi-Fi");
+}
+
+void loop() {
+  // Read temperature and humidity from DHT22
+  float temp_data = dht.readTemperature();
+  float hum_data = dht.readHumidity();
+
+  int data_ldr = analogRead(ldr);
+  int data_pir = digitalRead(pir);
+  float voltage = data_ldr / 4095.0 * 5.0;
+  float resistance = 2000 * voltage / (1 - voltage / 5.0);
+  float intensitas = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
+
+
+  // Check if any reads failed
+  if (isnan(temp_data) || isnan(hum_data)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  lcd.setCursor(0,0);
+  lcd.print("Temp = ");
+  lcd.print(temp_data);
+  lcd.setCursor(0,1);
+  lcd.print("Hum = ");
+  lcd.print(hum_data);
+
+  // Print values to serial monitor
+  Serial.print("Temperature: ");
+  Serial.print(temp_data);
+  Serial.print(" °C, Humidity: ");
+  Serial.print(hum_data);
+  Serial.println(" %");
+  Serial.print("Intensitas Cahaya : ");
+  Serial.print(intensitas);
+  Serial.println(" lux");
+
+  if(temp_data >= 40){
+      digitalWrite(Relay_AC, HIGH);
+      Serial.println("AC Menyala");
+  }
+  else{
+    digitalWrite(Relay_AC,LOW);
+    Serial.println("AC Mati");
+  }
+
+  if(intensitas > 400){
+    digitalWrite(Relay_Lamp, LOW);
+    Serial.println("Lampu Mati");
+  }
+  else{
+    digitalWrite(Relay_Lamp, HIGH);
+    Serial.println("Lampu Menyala");
+  }
+
+  if(data_pir == 1){
+    Serial.println("Terdapat Pengunjung");
+    pengunjung++;
+    Serial.print("Jumlah Pengunjung :");
+    Serial.println(pengunjung);
+  }
+  else{
+    Serial.print("Jumlah Pengunjung :");
+    Serial.println(pengunjung);
+  }
+
+  // Send data to server
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "raw/json");
+
+    String payload = "{\"temperature\":" + String(temp_data) + ",\"humidity\":" + String(hum_data) + ",\"Lux\":" + String(intensitas) + ",\"pengunjung\":" + String(pengunjung) + "}";
+
+    int httpResponseCode = http.POST(payload);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println("HTTP Response code: " + String(httpResponseCode));
+      Serial.println("Response: " + response);
+    } else {
+      Serial.println("Error on sending POST: " + String(httpResponseCode));
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+
+  Serial.println("----------------------------------------------");
+  // Send a request every 10 seconds
+  delay(10000);
+}
